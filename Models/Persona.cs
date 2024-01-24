4581f919be21963ff18c56db34481aa379233ab2
Damian <xdamian1jv@hotmail.com>
2024-01-23 20:54:43 -0500

    Agregar archivos de proyecto.

---
+++ Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace iandradeTS5.Models
+{
+    public class Persona
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(25), Unique]
+        public string Name { get; set; }
+    }
+}
---
+++ Vistas/vPrincipal.xaml.cs
@@ -0,0 +1,66 @@
+using iandradeTS5.Models;
+
+namespace iandradeTS5.Vistas;
+
+public partial class vPrincipal : ContentPage
+{
+    private int _id = 0;
+    public vPrincipal()
+	{
+		InitializeComponent();
+	}
+    private void btnInsertar_Clicked(object sender, EventArgs e)
+    {
+        statusMessaje.Text = "";
+        if (_id == 0)
+        {
+
+            App.personRepo.AddNewPerson(txtNombre.Text);
+            statusMessaje.Text = App.personRepo.StatusMessage;
+
+        }
+        else
+        {
+            App.personRepo.UpdatePerson(new Persona
+            {
+                Id = _id,
+                Name = txtNombre.Text
+            });
+            statusMessaje.Text = App.personRepo.StatusMessage;
+            _id = 0;
+        }
+
+        txtNombre.Text = string.Empty;
+        List<Persona> personas = App.personRepo.GetAllPepople();
+        listViw.ItemsSource = personas;
+        _id = 0;
+    }
+
+    private void btnMostrar_Clicked(object sender, EventArgs e)
+    {
+        statusMessaje.Text = "";
+        List<Persona> personas = App.personRepo.GetAllPepople();
+        listViw.ItemsSource = personas;
+    }
+
+    private async void listViw_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        var persona = (Persona)e.Item;
+        var action = await DisplayActionSheet("Action", "Cancel", null, "Editar", "Eliminar");
+
+
+        switch (action)
+        {
+            case "Editar":
+                _id = persona.Id;
+                txtNombre.Text = persona.Name;
+                break;
+            case "Eliminar":
+                App.personRepo.DeletePerson(persona);
+                statusMessaje.Text = App.personRepo.StatusMessage;
+                List<Persona> personas = App.personRepo.GetAllPepople();
+                listViw.ItemsSource = personas;
+                break;
+        }
+    }
+}
